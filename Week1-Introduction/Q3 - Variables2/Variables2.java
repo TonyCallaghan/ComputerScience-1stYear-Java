1429a8ac00bbc2764fbf73da2560f4aca15c1ab8
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2021-03-28 11:39:35 +0100

    Create Variables2.java

---
+++ Week1-Introduction/Q3 - Variables2/Variables2.java
@@ -0,0 +1,13 @@
+public class Variables2 {
+    
+    static int num1 = 5; //global variable = can be accessed in any method
+    
+     public static void main(String []args){
+        double num2 = 2.3;
+        char letter = 's';
+        
+        System.out.println(num1);
+        System.out.println(num2 +"\n" + letter);
+        // "\n" creates a new line within print statement.
+     }
+}
