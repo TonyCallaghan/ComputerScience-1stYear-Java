aeb981fcaa19c0b29b85a277e0d6100d96c23697
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2021-03-28 12:05:21 +0100

    Create Modulus.java

---
+++ Week1-Introduction/Modulus.java
@@ -0,0 +1,10 @@
+public class Modulus {
+
+     public static void main(String [] args) {
+        int num = 123;
+        System.out.println("the digits in " + num + " are:");
+        System.out.println(num/100);
+        System.out.println((num/10) % 10);
+        System.out.println(num % 10);
+     }
+}
