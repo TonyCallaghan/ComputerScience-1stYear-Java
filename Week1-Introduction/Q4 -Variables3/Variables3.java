35a997071aa755d5f2a20b73e167b225883ab244
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2021-03-28 11:53:27 +0100

    Create Variables3.java

---
+++ Week1-Introduction/Q4 -Variables3/Variables3.java
@@ -0,0 +1,14 @@
+public class Variables3 {
+    
+     public static void main(String []args){
+        int num1 = 10;
+        int num2 = 5;
+        int result1 = 0, result2 = 0; //shorter way of declaring vars of same DataType
+        
+        result1 = num1 + num2;
+        result2 = num1 * num2;
+        double result3 = num1 / num2; // Initilising varibales using operators.
+        
+        System.out.println(result1 + "\n" + result2 + "\n" + result3);
+     }
+}
