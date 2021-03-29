ee74015757054c7dcbf4d55e407ab36003c40631
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2021-03-29 22:21:32 +0100

    Create Voting.java

---
+++ Week2-Operators/Q1 - Voting Age/Voting.java
@@ -0,0 +1,12 @@
+public class Voting {
+
+     public static void main(String [] args) {
+        int age = 0;
+        
+        if (age<18) {
+            System.out.println("This person cannot vote");
+        } else (age>=18) {
+            System.out.println("This person can vote");
+        }
+    }
+}
