81f4a2939bf6a14d360dc8bd31d114f30da910ba
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2021-04-07 15:17:29 +0100

    Create StringFunctions.java

---
+++ Week4-Strings/Q2 - Strings2/StringFunctions.java
@@ -0,0 +1,11 @@
+public class StringFunctions {
+
+     public static void main(String []args) {
+        String s1 = "Most guitars have six strings";
+        
+        System.out.println("String length: " + s1.length());
+        System.out.println("Position of first 'h': " + s1.indexOf('h') );
+        System.out.println(s1.toUpperCase() );
+        System.out.println(s1.toLowerCase());
+     }
+}
