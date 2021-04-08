a46e8e688651cf623b3c6f386e2ccb58282c90fc
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2021-04-08 13:23:11 +0100

    Create Compare.java

---
+++ Week5-UserInput/Q1 - Comparing Integers/Compare.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HelloWorld 
+{
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = input.nextInt();
+        
+        System.out.println("Enter second number: ");
+        int num2 = input.nextInt();
+        
+        if(num1 == num2) {
+            System.out.println(num1 + " = " + num2);
+        } else if (num1 > num2) {
+            System.out.println(num1 + " > " + num2);
+        } else {
+            System.out.println(num1 + " < " + num2);
+        }
+    }
+}
+  
